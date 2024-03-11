72991de87940ce6cb8ed26f37b6d3123ec3e171b
Rohan Barman <rohanbarman100@gmail.com>
2024-03-11 23:53:58 +0530

    fixed cors proxy links

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const CLOUD_IMG_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const SWIGGY_API_URL =
-  "https://corsproxy.io/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D22.51800%26lng%3D88.38320%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING";
+  "https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D22.51800%26lng%3D88.38320%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING";
 
 export const SWIGGY_RESTAURANT_MENU_URL =
-  "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.51800&lng=88.38320&catalog_qa=undefined&submitAction=ENTER";
+  "https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D22.51800%26lng%3D88.38320%26catalog_qa%3Dundefined%26submitAction%3DENTER";
